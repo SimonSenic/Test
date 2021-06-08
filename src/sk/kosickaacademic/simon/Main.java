ed961441b35e3392e6986642efd8406a9e581af8
SimonSenic <simon.senic@kosickaakademia.sk>
2021-06-08 12:54:16 +0200

    Added new files

---
+++ src/sk/kosickaacademic/simon/Main.java
@@ -0,0 +1,11 @@
+package sk.kosickaacademic.simon;
+
+public class Main {
+    public static void main(String[] args) {
+        print();
+    }
+
+    public static void print(){
+        System.out.println("Pes");
+    }
+}
